214c14b9bb7a641cc1b2dc268bab559c775155ea
ashiq.cu.96@outlook.com <ashiq.cu.96@outlook.com>
2020-02-28 01:20:26 +0600

    importing project

---
+++ Asp.NetCoreMvcCURD/Models/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCoreMvcCURD.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        [Required(ErrorMessage ="This is required field")]
+        [DisplayName("Full Name")]
+     
+        public string FullName { get; set; }
+        [DisplayName("Emp. Code")]
+
+        public string EmpCode { get; set; }
+        [DisplayName("Designation")]
+
+        public string Position { get; set; }
+        [DisplayName("Office Location")]
+
+        public string OfficeLocation { get; set; }
+
+    }
+}
